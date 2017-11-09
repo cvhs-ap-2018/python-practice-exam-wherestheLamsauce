274c51fb266eedc924f702319ac0f1ae2708e4f2
Allen Thoe <30870017+MrThoe@users.noreply.github.com>
2017-11-09 08:38:59 -0800

    Add files via upload

---
+++ Python Exam/graphics.py
@@ -0,0 +1,29 @@
+"""
+1. Write the lines of code that would import
+and create a turtle named 'Pong'.
+"""
+
+
+"""
+2.  Draw a square with Pong of length 100
+"""
+
+
+
+"""
+3.  Write a function that will allow the user
+to input the length of a square and then draw
+a square of that length.
+"""
+
+
+"""
+4.  Draw a series of squares with different sizes
+that share the same corner.
+"""
+
+
+"""
+5.  Repeat the question above but change the color
+each time it draws a square.
+"""
