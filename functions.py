7630842b9dba67b35f2e235c2e66bd2afbbdbcc8
wherestheLamsauce <33526834+wherestheLamsauce@users.noreply.github.com>
2017-11-09 11:34:51 -0800

    Update functions.py

--- functions.py
+++ functions.py
@@ -2,14 +2,19 @@
 1.  Write a program that lists the numbers
     from 0-10 on separate lines
 """
-
+for i in range (11):
+    print (i)
 
 
 """
 2.  Write a program that counts down from
    100 by 5's
 """
-
+start = 100
+stop = 0
+iterate = -5
+for i in range (start, stop, iterate):
+    print (i)
 
 
 """
@@ -18,9 +23,10 @@ This Function should take the value x and
 Return 3 times that number. Call the function
 and print the result to the screen for x = 20
 """
-
-
-
+def Triple(x):
+    x = x*3
+    return x
+print (Triple(20))
 
 """
 4.  Write a program that will ask the user
@@ -28,8 +34,8 @@ to input a number, triple it and  display
 the result.  You will NEED print statements
 AND you may use your function above.
 """
-
-
+x = int(input('Give me a number '))
+print (Triple(x))
 
 """
 5.  Write a program that will return the result
